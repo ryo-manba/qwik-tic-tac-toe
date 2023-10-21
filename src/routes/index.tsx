891d77b40d661571e37f960808b9c74ad89675d4
Ryo Matsukawa <matsukawa.ryo42@gmail.com>
2023-10-21 14:10:21 +0900

    add: building-the-board

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
-export default component$(() => {
-  return <div>Hello world</div>;
-});
-
 export const head: DocumentHead = {
   title: "Qwik Tic Tac Toe",
   meta: [
@@ -14,3 +10,33 @@ export const head: DocumentHead = {
     },
   ],
 };
+
+export default component$(() => {
+  return (
+    <>
+      <div class="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div class="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div class="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
+});
+
+interface SquareProps {
+  value: string;
+}
+
+const Square = component$<SquareProps>(({ value }) => {
+  return <button class="square">{value}</button>;
+});
